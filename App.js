2e275ca417a960874c47cf10d838c30c55e9fd29
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-08-18 09:44:13 +0300

    added simple drawer (custom open menu)

--- App.js
+++ App.js
@@ -1,33 +1,95 @@
 import React from 'react';
 
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {HomeScreen, DetailsScreen} from './screens';
 
-const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const DetailsStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({navigation}) => {
+  return (
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#fa8072',
+        },
+        headerTintColor: '#E8E8E8',
+        headerTitleStyle: {
+          fontWeight: '900',
+        },
+      }}>
+      <HomeStack.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          title: 'overview',
+          headerLeft: () => (
+            <Ionicons.Button
+              name="ios-menu"
+              size={25}
+              backgroundColor="#fa8072"
+              onPress={() => {
+                navigation.openDrawer();
+              }}
+            />
+          ),
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+};
+const DetailsStackScreen = ({navigation}) => {
+  return (
+    <DetailsStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#fa8072',
+        },
+        headerTintColor: '#E8E8E8',
+        headerTitleStyle: {
+          fontWeight: '900',
+        },
+      }}>
+      <DetailsStack.Screen
+        name="details"
+        component={DetailsScreen}
+        options={{
+          headerLeft: () => (
+            <Ionicons.Button
+              name="ios-menu"
+              size={25}
+              backgroundColor="#fa8072"
+              onPress={() => {
+                navigation.openDrawer();
+              }}
+            />
+          ),
+        }}
+      />
+    </DetailsStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      {/* <StatusBar barStyle="light-content" /> */}
+      <Drawer.Navigator
         screenOptions={{
-          headerStyle: {
-            backgroundColor: '#fa8072',
-          },
-          headerTintColor: '#E8E8E8',
-          headerTitleStyle: {
-            fontWeight: '900',
-          },
-        }}>
-        <Stack.Screen
-          name="home"
-          component={HomeScreen}
-          options={{title: 'overview'}}
-        />
-        <Stack.Screen name="details" component={DetailsScreen} />
-      </Stack.Navigator>
+          drawerType: 'slide',
+          headerShown: false,
+        }}
+        initialRouteName="home">
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Details" component={DetailsStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
